bbec26af9bf9d8e95932f28e397dc04f4b7a684c
Vitaliy S. Orlov <orlov0562@gmail.com>
2019-11-30 16:44:16 +0200

    Update services.action.php

--- html/services.action.php
+++ html/services.action.php
@@ -7,5 +7,6 @@
         <li><a href="adminer/index.php" target="_blank">Open adminer in new window</a></li>
         <li><a href="http://<?=$ip?>:8983/solr/" target="_blank">Open SOLR in new window</a></li>
         <li><a href="http://<?=$ip?>:9200/" target="_blank">Open Elasticsearch in new window</a></li>
+        <li><a href="http://<?=$ip?>:7990/" target="_blank">Open Bitbucket server in new window</a></li>
     </ul>
 </div>
