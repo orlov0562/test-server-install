c4c6e50124ad4a72315cad4b6f06242561640255
Vitaliy S. Orlov <orlov0562@gmail.com>
2019-11-30 12:27:17 +0200

    Update phpinfo.php

--- phpinfo.php
+++ phpinfo.php
@@ -1,7 +1,9 @@
 <?php
 
+    $ip = file_get_contents('https://md5calc.com/ip.plain');
+
     // Simple protection from bots
-    if (empty($_GET['info']) || $_GET['info']!=$_SERVER['REMOTE_ADDR']) {
+    if (empty($_GET['info']) || $_GET['info']!=$ip) {
         header("HTTP/1.0 404 Not Found");
         exit;
     }
