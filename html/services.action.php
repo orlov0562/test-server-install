70ca8186b248b712827da8f61bc9ea2a9a3f82fc
Vitaliy S. Orlov <orlov0562@gmail.com>
2019-11-30 17:03:10 +0200

    Update services.action.php

--- html/services.action.php
+++ html/services.action.php
@@ -6,6 +6,7 @@
     <ul>
         <li><a href="phpinfo.php" target="_blank">Open Phpinfo in new window</a></li>
         <li><a href="adminer/index.php" target="_blank">Open Adminer in new window</a></li>
+        <li><a href="http://<?=$ip?>:9002" target="_blank">Open Portainer in new window</a></li>
         <li><a href="http://<?=$ip?>:8983/solr/" target="_blank">Open SOLR in new window</a></li>
         <li><a href="http://<?=$ip?>:9200/" target="_blank">Open Elasticsearch in new window</a></li>
         <li><a href="http://<?=$ip?>:7990/" target="_blank">Open Bitbucket in new window</a></li>
