d0a1dfbdad3ff9c46defe9ad18c540a1d149068a
Vitaliy S. Orlov <orlov0562@gmail.com>
2019-11-30 16:52:53 +0200

    Update services.action.php

--- html/services.action.php
+++ html/services.action.php
@@ -4,9 +4,12 @@
 <h1>Services</h1>
 <div id="page-content">
     <ul>
-        <li><a href="adminer/index.php" target="_blank">Open adminer in new window</a></li>
+        <li><a href="phpinfo.php" target="_blank">Open Phpinfo in new window</a></li>
+        <li><a href="adminer/index.php" target="_blank">Open Adminer in new window</a></li>
         <li><a href="http://<?=$ip?>:8983/solr/" target="_blank">Open SOLR in new window</a></li>
         <li><a href="http://<?=$ip?>:9200/" target="_blank">Open Elasticsearch in new window</a></li>
-        <li><a href="http://<?=$ip?>:7990/" target="_blank">Open Bitbucket server in new window</a></li>
+        <li><a href="http://<?=$ip?>:7990/" target="_blank">Open Bitbucket in new window</a></li>
+        <li><a href="http://<?=$ip?>:8111/" target="_blank">Open Teamcity in new window</a></li>
+        
     </ul>
 </div>
