e34d8d83ce53f928b17f6d5b28bccfbbdf554478
Vitaliy S. Orlov <orlov0562@gmail.com>
2019-11-30 11:49:33 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -6,7 +6,7 @@ curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable edge" -y
 
 sudo apt-get install software-properties-common -y
-sudo add-apt-repository ppa:ondrej/php
+sudo add-apt-repository ppa:ondrej/php -y
 
 apt-get update
 apt-get upgrade
