99bced31062568027d2fa1f5fa6f6271f6ad6351
Vitaliy S. Orlov <orlov0562@gmail.com>
2019-11-30 17:48:43 +0200

    Update services.action.php

--- html/services.action.php
+++ html/services.action.php
@@ -4,15 +4,15 @@
 <h1>Services</h1>
 <div id="page-content">
     <ul>
-        <li><a href="phpinfo.php" target="_blank">Open Phpinfo in new window</a></li>
-        <li><a href="adminer/index.php" target="_blank">Open Adminer in new window</a></li>
+        <li>PHP Info: <a href="phpinfo.php" target="_blank">http://<?=$ip?>/phpinfo.php</a></li>
+        <li>Adminer: <a href="adminer/index.php" target="_blank">http://<?=$ip?>/adminer/</a></li>
     </ul>
-    <p>The next services you should start after install from the /opt/docker folder</p>
+    <p>The next services you should start after install from the <strong>/opt/docker</strong> folder</p>
     <ul>
-        <li><a href="http://<?=$ip?>:9002" target="_blank">Open Portainer in new window</a></li>
-        <li><a href="http://<?=$ip?>:8983/solr/" target="_blank">Open SOLR in new window</a></li>
-        <li><a href="http://<?=$ip?>:9200/" target="_blank">Open Elasticsearch in new window</a></li>
-        <li><a href="http://<?=$ip?>:7990/" target="_blank">Open Bitbucket in new window</a></li>
-        <li><a href="http://<?=$ip?>:8111/" target="_blank">Open Teamcity in new window</a></li>
+        <li>Portainer: <a href="http://<?=$ip?>:9002" target="_blank">http://<?=$ip?>:9002</a></li>
+        <li>SOLR: <a href="http://<?=$ip?>:8983/solr/" target="_blank">http://<?=$ip?>:8983</a></li>
+        <li>Elasticsearch: <a href="http://<?=$ip?>:9200/" target="_blank">http://<?=$ip?>:9200</a></li>
+        <li>Bitbucket: <a href="http://<?=$ip?>:7990/" target="_blank">http://<?=$ip?>:7990</a></li>
+        <li>Teamcity: <a href="http://<?=$ip?>:8111/" target="_blank">http://<?=$ip?>:8111</a></li>
     </ul>
 </div>
