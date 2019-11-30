885d710fc5c4a9be1ed0413525980f3c10cb5d3e
Vitaliy S. Orlov <orlov0562@gmail.com>
2019-11-30 14:32:32 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -21,6 +21,7 @@ php7.4-sqlite3 php7.4-tidy php7.4-xml php7.4-zip php-amqp php-apcu php-gearman p
 php-mongodb php-oauth php-smbclient php-solr php-ssh2 php-uploadprogress php-uuid php-yaml php-xdebug
 
 cp ./php-fpm-php.ini /etc/php/7.4/fpm/php.ini
+cp ./php-xdebug.ini /etc/php/7.4/mods-available/xdebug.ini
 systemctl restart php7.4-fpm
 
 cp ./nginx-default.conf /etc/nginx/sites-available/default
@@ -31,7 +32,9 @@ mkdir -p /opt/docker
 cp ./phpinfo.php /var/www/html/phpinfo.php
 echo "-----------------------------"
 IP=$(curl -s https://md5calc.com/ip.plain)
-echo "PHP info: http://$IP/phpinfo.php?info=$IP"
-echo "MOUNT SSHFS: mkdir -p /mnt/remote/$IP && sshfs -o allow_other root@$IP:/ /mnt/remote/$IP"
-echo "XDEBUG TUNNEL: ssh -R 9999:localhost:9000 root@$IP"
+echo '' > /root/server-info.txt
+echo "IP: $IP" >> /root/server-info.txt
+echo "PHP info: http://$IP/phpinfo.php?info=$IP" >> /root/server-info.txt
+echo "MOUNT SSHFS: mkdir -p /mnt/remote/$IP && sshfs -o allow_other root@$IP:/ /mnt/remote/$IP" >> /root/server-info.txt
+echo "XDEBUG TUNNEL: ssh -R 9000:127.0.0.1:9999 root@$IP" >> /root/server-info.txt
 echo "-----------------------------"
