714cd524118c9bffffb89d4012094507d285c9f8
Vitaliy S. Orlov <orlov0562@gmail.com>
2019-11-30 12:10:19 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-mkdir -p /root/tmp && cd /root/tmp
-
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable edge" -y
 
